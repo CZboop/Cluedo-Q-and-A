dbc489c94ee44be1674446f24841e97035b0d1af
Branchey <70140890+CZboop@users.noreply.github.com>
2022-02-01 11:31:03 +0000

    loaded model and started on setup of states needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as qna from '@tensorflow-models/qna';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [model, setModel] = useState("");
+  const [info, setInfo] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  
+  const loadModel = async () => {
+    const qnaModel = await qna.load();
+    setModel(qnaModel);
+  }
+
+  useEffect(() => {
+    loadModel();
+  }, [])
+
   return (
     <div className="App">
-      
+      <h1>Question and Answer Bot</h1>
     </div>
   );
 }
