7bd99229be867c248cbe82cdf9767ce113404791
Branchey <70140890+CZboop@users.noreply.github.com>
2022-02-01 11:47:44 +0000

    added some question answering logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as qna from '@tensorflow-models/qna';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
   const [model, setModel] = useState("");
-  const [info, setInfo] = useState("");
-  const [question, setQuestion] = useState("");
+  const infoRef = useRef(null);
+  const questionRef = useRef(null);
   const [answer, setAnswer] = useState("");
   
   const loadModel = async () => {
     const qnaModel = await qna.load();
     setModel(qnaModel);
+    // console.log(model)
+  }
+
+  const answerQ = async (event) => {
+    if (model !== null && event.keyCode === '13'){
+      console.log("submitted")
+      const info = infoRef.current.value;
+      const question = questionRef.current.value;
+
+      const answers = await model.findAnswers(question, info);
+      setAnswer(answers);
+    }
+
   }
 
   useEffect(() => {
